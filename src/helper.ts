833f37fe79ba0362cb1d8a13af4ff0f044473455
ndri <5224431+ndri@users.noreply.github.com>
2024-08-29 18:05:22 +0200

    Disable checking whether config file exists
    This made the extension not work if rubocop is inside a Docker
    container.

--- src/helper.ts
+++ src/helper.ts
@@ -18,27 +18,8 @@ export function getCommandArguments(fileName: string): string[] {
   let commandArguments = ['--stdin', fileName, '--force-exclusion'];
   const extensionConfig = getConfig();
   if (extensionConfig.configFilePath !== '') {
-    const found = [extensionConfig.configFilePath]
-      .concat(
-        (vscode.workspace.workspaceFolders || []).map((ws) =>
-          path.join(ws.uri.path, extensionConfig.configFilePath)
-        )
-      )
-      .filter((p: string) => fs.existsSync(p));
-
-    if (found.length == 0) {
-      vscode.window.showWarningMessage(
-        `${extensionConfig.configFilePath} file does not exist. Ignoring...`
-      );
-    } else {
-      if (found.length > 1) {
-        vscode.window.showWarningMessage(
-          `Found multiple files (${found}) will use ${found[0]}`
-        );
-      }
-      const config = ['--config', found[0]];
-      commandArguments = commandArguments.concat(config);
-    }
+    const config = ['--config', extensionConfig.configFilePath];
+    commandArguments = commandArguments.concat(config);
   }
 
   return commandArguments;
